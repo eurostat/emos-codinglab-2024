860f0a8e359d6cb712ee291b37c241351784fc46
Your Name <your@users.noreply.github.com>
2024-04-22 17:15:55 +0200

    cultural employment - structure

---
+++ code/cultural_empl.R
@@ -0,0 +1,68 @@
+#############################################################
+# Article "Culture statistics - cultural employment"
+# URL: https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Culture_statistics_-_cultural_employment
+#############################################################
+
+# install and load packages
+install.packages("pacman")
+library(pacman)      # for package loading with "p_load"
+
+p_load(restatapi,    # for Eurostat data
+       giscoR,       # for Eurostat geographical Data
+       ggplot2,      # for graphic design
+       highcharter   # for interactive elements in graphics
+)
+
+
+#load data for article "Culture statistics - cultural employment"
+cultural_empl <- get_eurostat_data("cult_emp_sex")
+
+
+####################################
+# Map 1 - interactive - Nikola
+###################################
+
+
+
+####################################
+# Figure 1 - Pietro
+####################################
+
+
+
+
+###################################
+# Table 1 - Jose
+###################################
+
+
+
+###################################
+# Table 2
+####################################
+
+
+###################################
+# Table 3
+###################################
+
+
+###################################
+# Table 4
+###################################
+
+
+
+
+
+###################################
+# Table 2
+###################################
+
+
+
+
+###################################
+# Table 3
+###################################
+
