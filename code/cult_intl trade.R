52c35c96862f18c4535ab1b50dbd6dff106133d2
Rahul Kunte <rahulkunte@users.noreply.github.com>
2024-04-28 10:54:53 +0000

    changed file

--- code/cult_intl trade.R
+++ code/cult_intl trade.R
@@ -5,4 +5,23 @@
 
 library(restatapi)
 id <- "cult_trd_prd"
-date_filter
+
+
+#### Table 1 filters ####
+
+tab1_dates <- c(2017,2022) 
+tab1_country_keys <- c("EU27_2020", "EU28")
+tab1_partner_keys <- c("EXT_EU27_2020", "EXT_EU28")
+
+
+# Retrieve filtered data
+
+tab1_data <- get_eurostat_data(id,
+                               filters = list(geo = tab1_country_keys, 
+                                              partner = tab1_partner_keys),
+                               date_filter = tab1_dates,
+                               label = T
+)
+
+
+
