f4c69f3bff0fe8ef92b323f4c5eb2750898f52f9
Rahul Kunte <rahulkunte@users.noreply.github.com>
2024-04-28 15:04:00 +0000

    creating and formatting the table data for table 1

--- code/cult_intl trade.R
+++ code/cult_intl trade.R
@@ -4,6 +4,10 @@
 
 
 library(restatapi)
+library(kableExtra)
+library(dplyr)
+library(tidyr) # to pivot tables
+
 id <- "cult_trd_prd"
 
 
@@ -12,8 +16,11 @@ id <- "cult_trd_prd"
 # Filters
 
 tab1_dates <- c(2017,2022) 
-tab1_country_keys <- c("EU27_2020", "EU28")
-tab1_partner_keys <- c("EXT_EU27_2020", "EXT_EU28")
+tab1_country_keys <- c("EU27_2020") # , "EU28") 
+# Seems like EU27_2020 is sufficient because it has values before 2020 also,
+# possibly with deduction of values from Great Britain.
+
+tab1_partner_keys <- c("EXT_EU27_2020") #, "EXT_EU28")
 
 
 # Retrieve filtered data
@@ -26,7 +33,54 @@ tab1_data <- get_eurostat_data(id,
 
 
 
+# Creating the table
 
+# Lets first group the results for each category and time to 
+# make the creation process easier
+
+tab1_2017 <- tab1_data[tab1_data$time == 2017 & tab1_data$unit == "Thousand euro",]
+tab1_2022 <- tab1_data[tab1_data$time == 2022 & tab1_data$unit == "Thousand euro",]
+
+tab1_2017 <- tab1_2017 %>%
+             pivot_wider( names_from = stk_flow,
+                          values_from = values,
+                          values_fn = list(values = first)
+                        ) %>%
+             mutate(Trade_balance = Exports - Imports,
+                    Cover_ratio = Exports/Imports) %>%
+             select(-c(time, unit, partner, geo))
+    
+tab1_2022 <- tab1_2022 %>%
+  pivot_wider( names_from = stk_flow,
+               values_from = values,
+               values_fn = list(values = first)
+  ) %>%
+  mutate(Trade_balance = Exports - Imports,
+         Cover_ratio = Exports/Imports) %>%
+  select(-c(time, unit, partner, geo))   
+
+tab1_combined <- merge(tab1_2017, tab1_2022, by="prod_ct", suffixes = c("_2017", "_2022"))
+
+
+tab1_combined <- tab1_combined %>%
+                 mutate(Rel_ch_ex = round(100* (Exports_2022 - Exports_2017) / Exports_2017,1),
+                        Rel_ch_im = round(100*(Imports_2022 - Imports_2017) / Imports_2017,1)
+                        )
+
+# Rounding values to fit the table format
+
+tab1_combined <- tab1_combined %>%
+                 mutate(Exports_2017 = round(Exports_2017/1000,1),
+                        Imports_2017 = round(Imports_2017/1000,1),
+                        Trade_balance_2017 = round(Trade_balance_2017/1000, 1),
+                        Cover_ratio_2017 = round(Cover_ratio_2017, 2),
+                        
+                        Exports_2022 = round(Exports_2022/1000,1),
+                        Imports_2022 = round(Imports_2022/1000,1),
+                        Trade_balance_2022 = round(Trade_balance_2022/1000, 1),
+                        Cover_ratio_2022 = round(Cover_ratio_2022, 2)
+                        )
+                                                                  
 
 #### Figure 1: Evolution of extra-EU imports and exports in cultural goods and in total goods, EU, 2011-2022 ####
 
@@ -68,7 +122,4 @@ tab2_dates <- c(2017,2022)
 tab2_data <- get_eurostat_data(tab2_id,
                                date_filter = tab2_dates,
                                label = T)
-
-
-
 
