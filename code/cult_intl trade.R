59a4fc984b11ced3213e6b9e094bb0153d8756fc
Rahul <rahulkunte7@gmail.com>
2024-04-28 10:36:51 +0000

    intermediate changes

--- code/cult_intl trade.R
+++ code/cult_intl trade.R
@@ -1,2 +1,8 @@
+# Article 1
+# Culture statistics - international trade in cultural goods
+# https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Culture_statistics_-_international_trade_in_cultural_goods
+
+
 library(restatapi)
 id <- "cult_trd_prd"
+date_filter
