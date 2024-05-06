6ae4fe2ede79b36525e0022290b9ee481d3e4916
wendy-dw <74073915+wendy-dw@users.noreply.github.com>
2024-05-06 23:37:54 +0000

    draft versions

---
+++ Quality of Life_Polution_Income.R
@@ -0,0 +1,32 @@
+#Recommended Packages
+library(restatapi)
+library(giscoR)
+
+library(data.table)
+library(chron)
+library(reshape2)
+
+library(kableExtra)
+library(ggplot2)
+library(tmap)
+
+library(highcharter)
+library(plotly)
+
+#Quality of Life Indicators: Reported Noise from Neighbours grouped by income
+
+id <- get_eurostat_data("ilc_mddw01",date_filter=2020,flags=T)
+names(id)
+
+#ordering and filtering of data
+id <- subset(id, !(geo %in% c("EA18", "EA19")))
+id <- id[order(-id$values), ]
+id$geo <- factor(id$geo, levels = unique(id$geo))
+
+#plotting
+ggplot(id, aes(x = geo, y = values, fill = incgrp)) +
+  theme_minimal() +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Population reporting noise from neighbours or from the street, by income situation, 2020")
+
+
