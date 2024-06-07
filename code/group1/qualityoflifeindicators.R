087ecacf3d9aeb52dccce3eb06e32e3cd41a9d32
gmanuti <manutigabriella@gmail.com>
2024-06-07 10:02:34 +0000

    updated code

--- code/group1/qualityoflifeindicators.R
+++ code/group1/qualityoflifeindicators.R
@@ -4,33 +4,31 @@ library(restatapi)
 library(ggplot2)
 library(reshape2)
 
+#https://ec.europa.eu/eurostat/statistics-explained/index.php?title=Quality_of_life_indicators_-_overall_experience_of_life#EU_citizens_were_rather_satisfied_with_their_lives_in_2022
+
 #get the dataset
-ilc_pw01 <- get_eurostat_data("ilc_pw01", filters = c(unit = "RTG", isced11 = "TOTAL", indic_wb = "LIFESAT"))
+dsd<- get_eurostat_dsd("ilc_pw01")[concept=='geo']
 
-eu_iso_a2 <- c("AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "EL" ,"ES", "FI","FR", "HR", "HU", "IE", "IT", "LT", "LU", "LV", "MT", "NL", "PL", "PT", 
-               "RO", "SE", "SI", "SK" ,"EU27_2020")
-
-ilc_pw01 <- ilc_pw01[(ilc_pw01$geo %in% eu_iso_a2), ]
-
-country_names <- toupper(c("AT" = "Austria", "BE" = "Belgium", "BG" = "Bulgaria", "CY" = "Cyprus", "CZ" = "Czech Republic", "DE" = "Germany", "DK" = "Denmark", "EE" = "Estonia", "EL" = "Greece", "ES" = "Spain", "FI" = "Finland", "FR" = "France", "HR" = "Croatia", "HU" = "Hungary", "IE" = "Ireland", "IT" = "Italy", "LT" = "Lithuania", "LU" = "Luxembourg", "LV" = "Latvia", "MT" = "Malta", "NL" = "Netherlands", "PL" = "Poland", "PT" = "Portugal", "RO" = "Romania", "SE" = "Sweden","SI" = "Slovenia", "SK" = "Slovakia"))
+ilc_pw01 <- get_eurostat_data("ilc_pw01", filters = c(unit = "RTG", isced11 = "TOTAL", indic_wb = "LIFESAT"), label=TRUE)
 
 
 #Figure 1
 #Satisfaction by country
-countries <- subset(ilc_pw01, sex == "T" & age == "Y_GE16" & time == "2022" & geo != "EU27_2020")
 
-countries2<- subset(ilc_pw01, sex == "T" & age == "Y_GE16" & time == "2022")
+excluded_countries <- c("European Union - 27 countries (from 2020)","Switzerland", "Norway", "Serbia", "Montenegro", "Türkiye")
 
-filtered_countries <- countries[countries$geo %in% names(country_names), ]
+countries <- subset(ilc_pw01, sex == "Total" & age == "16 years or over" & time == "2022" & !(geo %in% excluded_countries))
 
-EU_value=countries2$values[countries2$geo== "EU27_2020"]
+countries2<- subset(ilc_pw01, sex == "Total" & age == "16 years or over" & time == "2022")
 
-plot0 <- ggplot(countries, aes(x = reorder(geo, values[geo != "EU27_2020"], decreasing = TRUE), y = values)) +
+EU_value=countries2$values[countries2$geo== "European Union - 27 countries (from 2020)"]
+
+plot0 <- ggplot(countries, aes(x = reorder(geo, values[geo != "European Union - 27 countries (from 2020)"], decreasing = TRUE), y = values)) +
   geom_col(width = 0.6, fill = "#2644A7") +
-  geom_hline(yintercept = EU_value, color = "#F8AE21",linetype = "solid",linewidth =1) +
+  geom_hline(yintercept = EU_value, color = "#B09120",linetype = "solid",linewidth =1) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 10),breaks = seq(0, 10, by = 1)) +
   #scale_x_discrete(labels = country_names[unique(countries$geo)]) +
-  scale_x_discrete(labels = country_names[unique(toupper(filtered_countries$geo))]) +
+  scale_x_discrete(labels = countries$geo) +
   coord_cartesian(ylim = c(0, 10)) +
   #theme_minimal() +
   theme(panel.grid.major.y = element_line(color = "gray", linetype = "dashed"),         panel.grid.minor = element_blank(),
@@ -49,11 +47,50 @@ plot0 <- ggplot(countries, aes(x = reorder(geo, values[geo != "EU27_2020"], decr
 
 print(plot0)
 
+#save the plot
+library(openxlsx)
+
+ggsave("my_plot.png", plot = plot0, width = 8, height = 6, dpi = 300)
+
+#cant use the following code because the plot is not in a data.frame
+write.xlsx(plot0, file = "plot0.xlsx", sheetName = "Plot0", rowNames = FALSE)
+
+
+
 
 #Map 1
 #Overall life satisfaction by age group, 2022
+library(giscoR)
+library(tmap)
+
+ilc_pw01_map <- get_eurostat_data("ilc_pw01", filters = c(unit = "RTG", isced11 = "TOTAL", indic_wb = "LIFESAT"))
+
+excluded_countries_map <- c("EU27_2020","CH", "NO", "RS", "ME", "TR")
+
+countries_map <- ilc_pw01_map %>%
+  filter(sex == "T" & age %in% c("Y16-19", "Y_GE65") & time == 2022 & !(geo %in% excluded_countries_map))
+
+countries_diff <- countries_map %>%
+  pivot_wider(names_from = age, values_from = values) %>%
+  mutate(difference = `Y16-19` - `Y_GE65`)
+
+map <- gisco_get_nuts(resolution = "10", nuts_level = "0", year = "2021")
+
+merged_data <- map %>%
+  left_join(countries_diff, by = c("NUTS_ID" = "geo"))
 
 
+map1 <- tm_shape(merged_data) +
+  tm_polygons("difference", 
+              title = "Overall life satisfaction by age group, 2022", 
+              subtitle= "Difference between the satisfaction level",
+              palette = "RdYlBu",
+              midpoint=NA,
+              breaks = c(-Inf, -0.3, 0, 0.3, 1, Inf)) +
+  tm_layout(legend.outside = FALSE)
+
+
+print(map1)
 
 
 
@@ -63,74 +100,85 @@ print(plot0)
 library(dplyr)
 library(tidyr)
 
-ilc_pw01_2 <- get_eurostat_data("ilc_pw01", filters = c(unit = "RTG", indic_wb = "LIFESAT"))
+ilc_pw01_2 <- get_eurostat_data("ilc_pw01", filters = c(unit = "RTG", indic_wb = "LIFESAT"),label=TRUE)
 
-eu_iso_a2 <- c("AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "EL" ,"ES", "FI","FR", "HR", "HU", "IE", "IT", "LT", "LU", "LV", "MT", "NL", "PL", "PT", 
-               "RO", "SE", "SI", "SK" ,"EU27_2020")
+excluded_countries2 <- c("Switzerland", "Norway", "Serbia", "Montenegro", "Türkiye", "Kosovo*")
 
-ilc_pw01_2 <- ilc_pw01_2[(ilc_pw01_2$geo %in% eu_iso_a2), ]
+education_countries <- subset(ilc_pw01_2, sex == "Total" & age == "16 years or over" & time == "2022" & isced11 != "All ISCED 2011 levels" & !(geo %in% excluded_countries2))
 
-country_names <- toupper(c("AT" = "Austria", "BE" = "Belgium", "BG" = "Bulgaria", "CY" = "Cyprus", "CZ" = "Czech Republic", "DE" = "Germany", "DK" = "Denmark", "EE" = "Estonia", "EL" = "Greece", "ES" = "Spain", "FI" = "Finland", "FR" = "France", "HR" = "Croatia", "HU" = "Hungary", "IE" = "Ireland", "IT" = "Italy", "LT" = "Lithuania", "LU" = "Luxembourg", "LV" = "Latvia", "MT" = "Malta", "NL" = "Netherlands", "PL" = "Poland", "PT" = "Portugal", "RO" = "Romania", "SE" = "Sweden","SI" = "Slovenia", "SK" = "Slovakia"))
+custom_colors <- c("Less than primary, primary and lower secondary education (levels 0-2)" = "#F8AE21","Upper secondary and post-secondary non-tertiary education (levels 3 and 4)" = "yellow",  "Tertiary education (levels 5-8)" = "#2644A7") 
 
-#filtered_countries <- countries[countries$geo %in% names(country_names), ]
 
-education_countries <- subset(ilc_pw01_2, sex == "T" & age == "Y_GE16" & time == "2022" & isced11 != "TOTAL" )
+eu_data <- subset(education_countries, geo == "European Union - 27 countries (from 2020)")
+country_data <- subset(education_countries, geo != "European Union - 27 countries (from 2020)")
 
-custom_colors <- c("ED0-2" = "#F8AE21","ED3_4" = "yellow",  "ED5-8" = "#2644A7") 
 
-#try to put EU level first
-#education_countries$geo <- factor(education_countries$geo, levels = c("EU27_2020", levels(education_countries$geo)))
-#try to order the life satisfaction rate by the tertiary education 
+country_data <- country_data %>%
+  arrange(desc(ifelse(isced11 == "Tertiary education (levels 5-8)", values, NA)))
 
-plot2<- ggplot(education_countries, aes(x = reorder(geo, desc(isced11 == "ED5-8")), y = values, fill = isced11)) +
+ordered_data <- bind_rows(eu_data, country_data)
+
+country_names <- unique(ordered_data$geo)
+
+
+
+plot2 <- ggplot(ordered_data, aes(x = factor(geo, levels = country_names), y = values, fill = isced11)) +
   scale_fill_manual(values = custom_colors, labels = c("Less than secondary education", "Secondary education", "Tertiary education")) +
-  geom_bar(stat = "identity",position = position_dodge(width = 0.6), width=0.4) +
-  labs(title = "Life satisfaction by level of education,2022",x = NULL ,y = NULL, fill = "Education Level") +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 9),breaks = seq(0, 9, by = 1)) +
-  scale_x_discrete(labels = country_names[unique(toupper(filtered_countries$geo))]) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.6), width = 0.4) +
+  labs(title = "Life satisfaction by level of education, 2022", x = NULL, y = NULL, fill = "Education Level") +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 9), breaks = seq(0, 9, by = 1)) +
   coord_cartesian(ylim = c(0, 9)) +
   theme(panel.grid.major.y = element_line(color = "gray", linetype = "dashed"),
         plot.title = element_text(hjust = 0.5), 
         panel.background = element_rect(fill = "white"), 
         panel.grid.major.x = element_blank(),
         panel.grid.minor = element_blank(), 
-        axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1, color="black"),
-        axis.text.y = element_text(vjust = 0.5, hjust = 0, color="black"),
+        axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1, color = "black"),
+        axis.text.y = element_text(vjust = 0.5, hjust = 0, color = "black"),
         legend.position = "bottom",     
         legend.title = element_text(face = "bold"),
         axis.ticks = element_line(),
-        axis.ticks.length.y  = unit(0, "cm"),
-        axis.line.x = element_line(colour = "black", linetype = "solid"))+
-  guides(fill = guide_legend(override.aes = list(size = 1)))+
-  scale_shape_manual(values = 16)
+        axis.ticks.length.y = unit(0, "cm"),
+        axis.line.x = element_line(colour = "black", linetype = "solid")) +
+  guides(fill = guide_legend(override.aes = list(size = 1))) +
+  scale_shape_manual(values = 16)+
+  scale_x_discrete(labels = function(x) ifelse(x == 'European Union - 27 countries (from 2020)', 'EU', x))
 
 
 print(plot2)
 
 
+
+
+
 #Figure 3
 #Overall life satisfaction by level of urbanisation 2022
 
-ilc_pw02 <- get_eurostat_data("ilc_pw02", filters = c(unit = "RTG", indic_wb = "LIFESAT"))
+ilc_pw02 <- get_eurostat_data("ilc_pw02", filters = c(unit = "RTG", indic_wb = "LIFESAT"), label=TRUE)
 
-eu_iso_a2 <- c("AT", "BE", "BG", "CY", "CZ", "DE", "DK", "EE", "EL" ,"ES", "FI","FR", "HR", "HU", "IE", "IT", "LT", "LU", "LV", "MT", "NL", "PL", "PT", 
-               "RO", "SE", "SI", "SK" ,"EU27_2020")
+excluded_countries2 <- c("Switzerland", "Norway", "Serbia", "Montenegro", "Türkiye", "Kosovo*")
 
-ilc_pw02 <- ilc_pw02[(ilc_pw02$geo %in% eu_iso_a2), ]
+urbanisation_countries <- subset(ilc_pw02, time == "2022" & hhcomp== "Total" & quant_inc== "Total" & deg_urb != "Total" & !(geo %in% excluded_countries2) )
 
-country_names <- toupper(c("AT" = "Austria", "BE" = "Belgium", "BG" = "Bulgaria", "CY" = "Cyprus", "CZ" = "Czech Republic", "DE" = "Germany", "DK" = "Denmark", "EE" = "Estonia", "EL" = "Greece", "ES" = "Spain", "FI" = "Finland", "FR" = "France", "HR" = "Croatia", "HU" = "Hungary", "IE" = "Ireland", "IT" = "Italy", "LT" = "Lithuania", "LU" = "Luxembourg", "LV" = "Latvia", "MT" = "Malta", "NL" = "Netherlands", "PL" = "Poland", "PT" = "Portugal", "RO" = "Romania", "SE" = "Sweden","SI" = "Slovenia", "SK" = "Slovakia"))
+custom_colors4 <- c("Cities" = "#F8AE21","Towns and suburbs"= "yellow", "Rural areas" = "#2644A7") 
+eu_data2 <- subset(urbanisation_countries, geo == "European Union - 27 countries (from 2020)")
+country_data2 <- subset(urbanisation_countries, geo != "European Union - 27 countries (from 2020)")
 
 
-urbanisation_countries <- subset(ilc_pw02, time == "2022"& hhcomp== "TOTAL"& quant_inc== "TOTAL" & deg_urb != "TOTAL" )
+country_data2 <- country_data2 %>%
+  arrange(desc(ifelse(deg_urb == "Cities", values, NA)))
 
-custom_colors4 <- c("DEG1" = "#F8AE21","DEG2"= "yellow", "DEG3" = "#2644A7") 
+ordered_data2 <- bind_rows(eu_data2, country_data2)
 
-plot3<- ggplot(urbanisation_countries, aes(x = reorder(geo, desc(deg_urb == "DEG1")), y = values, fill = deg_urb)) +
+country_names2 <- unique(ordered_data2$geo)
+
+
+
+plot3<- ggplot(ordered_data2, aes(x = factor(geo, levels=country_names2), y = values, fill = deg_urb)) +
   scale_fill_manual(values = custom_colors4, labels = c("Cities", "Small Towns", "Rural Areas")) +
   geom_bar(stat = "identity",position = position_dodge(width = 0.6), width=0.4) +
   labs(title = "Overall life satisfaction by level of urbanisation,2022",x = NULL ,y = NULL, fill = "Urbanisation degree") +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 9),breaks = seq(0, 9, by = 1)) +
-  scale_x_discrete(labels = country_names[unique(toupper(filtered_countries$geo))]) +
   coord_cartesian(ylim = c(0, 9)) +
   theme(panel.grid.major.y = element_line(color = "gray", linetype = "dashed"),
         plot.title = element_text(hjust = 0.5), 
@@ -145,7 +193,9 @@ plot3<- ggplot(urbanisation_countries, aes(x = reorder(geo, desc(deg_urb == "DEG
         axis.ticks.length.y  = unit(0, "cm"),
         axis.line.x = element_line(colour = "black", linetype = "solid"))+
   guides(fill = guide_legend(override.aes = list(size = 1)))+
-  scale_shape_manual(values = 16)
+  scale_shape_manual(values = 16)+
+  scale_x_discrete(labels = function(x) ifelse(x == 'European Union - 27 countries (from 2020)', 'EU', x))
+
 
 
 print(plot3)
@@ -154,45 +204,76 @@ print(plot3)
 #Figure 4
 #Overall life satisfaction by demographic characteristics at EU level, 2018 and 2022
 
-#NOT COMPLETE
+library(reshape2)
 
-household_countries <- subset(ilc_pw02, time == c("2018","2022") & hhcomp != "TOTAL" & quant_inc != "TOTAL" & deg_urb == "TOTAL" )
+household_countries <- subset(ilc_pw02, time == c("2018","2022") & hhcomp != "Total" & quant_inc != "Total" & deg_urb == "Total" )
+
+hhcomp_data <- household_countries %>%
+  filter(hhcomp != "Total") %>%
+  mutate(category = hhcomp)
+
+quant_inc_data <- household_countries %>%
+  filter(quant_inc != "Total") %>%
+  mutate(category = quant_inc)
+
+combined_data <- bind_rows(hhcomp_data, quant_inc_data)
+
+mean_satisfaction <- combined_data %>%
+  group_by(category, time) %>%
+  summarize(mean_values = mean(values, na.rm = TRUE))
+
+satisfaction_wide <- dcast(mean_satisfaction, category ~ time, value.var = "mean_values")
+
+satisfaction_wide$category <- factor(satisfaction_wide$category, levels = c("One adult", "Two adults", "Three or more adults", "Households without dependent children", "Households with dependent children", "First quintile", "Second quintile", "Third quintile", "Fourth quintile", "Fifth quintile"))
 
 
-data_2018 <- subset(household_countries, time == "2018")
-data_2022 <- subset(household_countries, time == "2022")
-
-ggplot() +
-  geom_bar(data = data_2022, aes(x = quant_inc, y = values, fill =time),
-           stat = "identity", position = position_dodge(width = 0.4), width = 0.3) +
-  geom_bar(data = data_2022, aes(x = hhcomp, y = values, fill = time),
-           stat = "identity", position = position_dodge(width = 0.4), width = 0.3) +
-  geom_col(width = 0.6, fill = "gold3") +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 8),breaks = seq(0, 8, by = 1)) +
-  labs(title = "Overall life satisfaction by demographic characteristics at EU level, 2018 and 2022", x = NULL,y = NULL) +
+plot4 <- ggplot(satisfaction_wide, aes(x = category)) +
+  geom_col(aes(y = `2022`, fill = "2022"), width = 0.4) +
+  geom_point(aes(y = `2018`, color = "2018"), shape = 22, size = 3, fill = "blue") +
+  scale_fill_manual(values = c("2022" = "gold3")) +
+  scale_color_manual(values = c("2018" = "blue")) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 8), breaks = seq(0, 8, by = 1)) +
+  labs(title = "Overall life satisfaction by demographic characteristics at EU level, 2018 and 2022", 
+       x = NULL, y = NULL) +
   theme(panel.grid.major.y = element_line(color = "gray", linetype = "dashed"),
         plot.title = element_text(hjust = 0.5), 
         panel.background = element_rect(fill = "white"), 
         panel.grid.major.x = element_blank(),
         panel.grid.minor = element_blank(), 
-        axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1, color="black"),
-        axis.text.y = element_text(vjust = 0.5, hjust = 0, color="black"),
-        legend.position = "bottom",     
-        legend.title = element_text(face = "bold"),
-        axis.ticks = element_line(),
-        axis.ticks.length.y  = unit(0, "cm"),
-        axis.line.x = element_line(colour = "black", linetype = "solid"))+
-  guides(fill = guide_legend(override.aes = list(size = 1))
-         
-  )
+        axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1, color = "black"),
+        axis.text.y = element_text(vjust = 0.5, hjust = 0, color = "black"),
+        axis.line.x = element_line(colour = "black", linetype = "solid")) +
+  legend("bottom", legend = c("2018", "2022"), 
+         fill = c("blue", "gold3"), 
+         title = "Year")
+
+print(plot4)
 
 
 
+p <- ggplot(satisfaction_wide, aes(x = category))
 
+p <- p + geom_col(aes(y = `2022`, fill = "2022"), width = 0.4)
+p <- p + geom_point(aes(y = `2018`, color = "2018"), shape = 22, size = 3, fill = "blue")
 
+p <- p + scale_fill_manual(values = c("2022" = "gold3"))
+p <- p + scale_color_manual(values = c("2018" = "blue"))
 
+p <- p + scale_y_continuous(expand = c(0, 0), limits = c(0, 8), breaks = seq(0, 8, by = 1))
 
+p <- p + labs(title = "Overall life satisfaction by demographic characteristics at EU level, 2018 and 2022", 
+              x = NULL, y = NULL)
+p <- p + theme(panel.grid.major.y = element_line(color = "gray", linetype = "dashed"),
+               plot.title = element_text(hjust = 0.5), 
+               panel.background = element_rect(fill = "white"), 
+               panel.grid.major.x = element_blank(),
+               panel.grid.minor = element_blank(), 
+               axis.text.x = element_text(angle = 60, vjust = 1, hjust = 1, color = "black"),
+               axis.text.y = element_text(vjust = 0.5, hjust = 0, color = "black"),
+               axis.line.x = element_line(colour = "black", linetype = "solid"))
 
+p <- p + theme(legend.position = "bottom") +
+  guides(fill = guide_legend(title = "Year", title.position = "top", title.hjust = 0.5),
+         color = guide_legend(title = "Year", title.position = "top", title.hjust = 0.5))
 
-
-
+print(p)
